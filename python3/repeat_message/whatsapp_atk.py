b01127cf4148fae227e6ccec6a21321eb577bf77
renatoexpert <renatorraraujo@gmail.com>
2022-10-09 11:01:38 -0300

    letting some comments

--- python3/repeat_message/whatsapp_atk.py
+++ python3/repeat_message/whatsapp_atk.py
@@ -1,9 +1,9 @@
 import keyboard
 import time
 
-time.sleep(5)
+time.sleep(5)       #   Giving you enough time to select input box with mouse pointer
 while True:
     keyboard.press_and_release("ctrl+v,enter")
-    time.sleep(1)
+    time.sleep(1)   #   Turning sleep time too short or zero makes your PC freeze!
 
 
