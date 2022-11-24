0fda4a56d47b798ff5014276ac91f71b50c17392
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2022-11-24 06:21:13 -0300

    fix y not number

--- javascript/websocket/plotter.py
+++ javascript/websocket/plotter.py
@@ -7,24 +7,27 @@ dataframe = json.loads(raw_string)
 import matplotlib.pyplot as plt
 import numpy as np
 
-#plt.style.use('_mpl-gallery')
-
 b_ask_keys = list(dataframe['binance']['ask'].keys())
 b_ask_x = np.array([int(x) for x in b_ask_keys])
-b_ask_y = [dataframe['binance']['ask'][time] for time in b_ask_keys]
+b_ask_y = [float(dataframe['binance']['ask'][time]) for time in b_ask_keys]
 
 o_bid_keys = list(dataframe['okx']['bid'].keys())
 o_bid_x = np.array([int(x) for x in o_bid_keys])
-o_bid_y = [dataframe['okx']['bid'][time] for time in o_bid_keys]
+o_bid_y = [float(dataframe['okx']['bid'][time]) for time in o_bid_keys]
 
-fig, ax = plt.subplots()
-#ax.plot(b_ask_x, b_ask_y, o_bid_x, o_bid_y, linewidth=0.5)
+fig, ax = plt.subplots(sharex=True, sharey=True)
+ax.set_xticks(np.arange(0, 30000, 5000))
+ax.set_xticks(np.arange(0, 30000, 1000), minor=True)
+ax.set_yticks(np.arange(10000, 20000, 100))
+ax.set_yticks(np.arange(10000, 20000, 100), minor=True)
+ax.grid(True)
+
 ax.plot(b_ask_x, b_ask_y, linewidth=0.5, color='red')
 ax2 = ax.twinx()
 ax2.plot(o_bid_x, o_bid_y, linewidth=0.5, color='blue')
+
 ax.set_xlabel('Time')
 ax.set_ylabel('Price')
 ax.set_title('Binance asks versus Okx bids')
-#ax.grid(True)
 plt.show()
 
