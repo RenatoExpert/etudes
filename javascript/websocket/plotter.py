500e2baadf1f6ee4848568fb759b1075a68afaca
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2022-11-24 15:26:20 -0300

    let only fill_between axis

--- javascript/websocket/plotter.py
+++ javascript/websocket/plotter.py
@@ -27,39 +27,21 @@ print('Binance frames:' , len(b_x))
 
 fig, axs = plt.subplots(4, 1, sharex=True, sharey=True)
 
-# === Binance asks versus Okx bids ====
-axs[0].set_title('Binance asks versus Okx bids')
+# === Okx spread ===
+axs[0].set_title('Okx Spread')
 axs[0].grid(True)
 axs[0].set_xlabel('Time')
 axs[0].set_ylabel('Price')
-axs[0].plot(b_x, b_ya, linewidth=0.5, color='red')
-ax02 = axs[0].twinx()
-ax02.plot(o_x, o_yb, linewidth=0.5, color='blue')
-
-# === Binance asks versus Okx bids ====
-axs[1].set_title('Binance bids versus Okx asks')
-axs[1].grid(True)
-axs[1].set_xlabel('Time')
-axs[1].set_ylabel('Price')
-axs[1].plot(o_x, o_ya, linewidth=0.5, color='red')
-ax12 = axs[1].twinx()
-ax12.plot(b_x, b_yb, linewidth=0.5, color='blue')
-
-# === Okx spread ===
-axs[2].set_title('Okx Spread')
-axs[2].grid(True)
-axs[2].set_xlabel('Time')
-axs[2].set_ylabel('Price')
-axs[2].fill_between(o_x, o_ya, o_yb, linewidth=0.5, color='red')
+axs[0].fill_between(o_x, o_ya, o_yb, linewidth=0.5, color='red')
 #ax22 = axs[2].twinx()
 #ax22.plot(b_x, b_yb, linewidth=0.5, color='gray')
 
 # === Binance spread ===
-axs[3].set_title('Binance Spread')
-axs[3].grid(True)
-axs[3].set_xlabel('Time')
-axs[3].set_ylabel('Price')
-axs[3].fill_between(b_x, b_ya, b_yb, linewidth=0.5, color='yellow')
+axs[1].set_title('Binance Spread')
+axs[1].grid(True)
+axs[1].set_xlabel('Time')
+axs[1].set_ylabel('Price')
+axs[1].fill_between(b_x, b_ya, b_yb, linewidth=0.5, color='yellow')
 #ax22 = axs[3].twinx()
 #ax22.plot(b_x, b_yb, linewidth=0.5, color='blue')
 
