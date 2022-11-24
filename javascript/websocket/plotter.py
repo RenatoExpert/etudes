2a068ef98ea7908f754811a27edeb72c593265b9
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2022-11-24 04:41:38 -0300

    using integers in x instead of keys

--- javascript/websocket/plotter.py
+++ javascript/websocket/plotter.py
@@ -9,12 +9,15 @@ import numpy as np
 
 plt.style.use('_mpl-gallery')
 
-b_ask_x = dataframe['binance']['ask'].keys()
-b_ask_y = [dataframe['binance']['ask'][time] for time in b_ask_x]
+b_ask_keys = list(dataframe['binance']['ask'].keys())
+b_ask_x = [int(x) for x in b_ask_keys]
+b_ask_y = [dataframe['binance']['ask'][time] for time in b_ask_keys]
 
-o_bid_x = dataframe['okx']['bid'].keys()
-o_bid_y = [dataframe['okx']['bid'][time] for time in o_bid_x]
+o_bid_keys = list(dataframe['okx']['bid'].keys())
+o_bid_x = [int(x) for x in o_bid_keys]
+o_bid_y = [dataframe['okx']['bid'][time] for time in o_bid_keys]
 
+ref_x = b_ask_x + o_bid_x
 
 fig, ax = plt.subplots()
 #ax.plot(b_ask_x, b_ask_y, o_bid_x, o_bid_y, linewidth=0.5)
