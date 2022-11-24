5d8cdc66113a51eab86482604f88bd4b527ce89b
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2022-11-24 04:22:54 -0300

    adding another line

--- javascript/websocket/experiment.py
+++ javascript/websocket/experiment.py
@@ -12,7 +12,17 @@ plt.style.use('_mpl-gallery')
 b_ask_x = dataframe['binance']['ask'].keys()
 b_ask_y = [dataframe['binance']['ask'][time] for time in b_ask_x]
 
+o_bid_x = dataframe['okx']['bid'].keys()
+o_bid_y = [dataframe['okx']['bid'][time] for time in o_bid_x]
+
+
 fig, ax = plt.subplots()
+#ax.plot(b_ask_x, b_ask_y, o_bid_x, o_bid_y, linewidth=0.5)
 ax.plot(b_ask_x, b_ask_y, linewidth=0.5)
+ax2 = ax.twiny()
+ax2.plot(o_bid_x, o_bid_y, linewidth=0.5)
+ax.set_xlabel('Time')
+ax.set_ylabel('Binance ask and Okx Bid')
+ax.grid(True)
 plt.show()
 
